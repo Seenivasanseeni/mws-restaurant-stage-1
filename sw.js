dee86b78ca00f3746f4fed839d065740d8c4fb44
SeenivasanSeeni <seeni0424@gmail.com>
2018-10-27 16:11:24 +0530

    done basic cacheing

--- sw.js
+++ sw.js
@@ -1,3 +1,28 @@
 self.addEventListener('fetch',function(event){
     console.log("Fetch event for url:",event.request.url);
+    caches.open("cache-v1").then(function(cache){
+            //use match function 
+            cache.match(event.request).then(function(response){
+                if(response){
+                    console.log("Response was found in cache.");
+                    return response;
+                }
+                console.log("Fetching Resource from Network");
+                fetch(event.request).then(function(networkResponse){
+                    cache.put(event.request,networkResponse);
+                    return networkResponse;
+                }).catch(function(){
+                    console.log("Error in fetching the resource");
+                })
+            })
+    }).catch(function(){
+        console.log("Error in handling caches");
+    })
+})
+
+self.addEventListener('install',function(event){
+    console.log("install sw")
+})
+self.addEventListener('activate',function(event){
+    console.log("Activation of sw")
 })
