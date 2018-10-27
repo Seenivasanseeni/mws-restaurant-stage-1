d58861623900c48cc1a934e6ecb06a418c195160
SeenivasanSeeni <seeni0424@gmail.com>
2018-10-27 09:12:23 +0530

    responsiveness added

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoic2Vlbml2YXNhbnNlZW5pIiwiYSI6ImNqbmZ2YzBybjNpdm4zcW1zdWkyYjF6bzUifQ.TTyfTabrNwGkLqFKNkZMiQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
